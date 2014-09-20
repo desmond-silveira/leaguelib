735332b463227731fff2cf4668bcd04175a801af
Desmond Silveira <dsilveira@local.com>
2014-09-20 10:46:59 -0700

    Commented game constants based on Riot API.

--- src/main/java/com/achimala/leaguelib/models/LeagueMatchmakingQueue.java
+++ src/main/java/com/achimala/leaguelib/models/LeagueMatchmakingQueue.java
@@ -16,20 +16,49 @@
 
 package com.achimala.leaguelib.models;
 
+/**
+ * @see <a href="https://developer.riotgames.com/docs/game-constants">Game Constants</a>
+ */
 public enum LeagueMatchmakingQueue {
+    /** Custom games. */
     NONE,
-    BOT,
-    BOT_3x3,
+    /** Summoner's Rift unranked games. */
     NORMAL,
+    /** Twisted Treeline unranked games. */
     NORMAL_3x3,
+    /** Dominion/Crystal Scar games. */
     ODIN_UNRANKED,
-    RANKED_SOLO_5x5,
-    RANKED_TEAM_3x3,
-    RANKED_TEAM_5x5,
+    /** ARAM / Howling Abyss games. */
     ARAM_UNRANKED_5x5,
+    /**
+     * Summoner's Rift and Crystal Scar games played against Intro, Beginner, or
+     * Intermediate AI.
+     */
+    BOT,
+    /** Twisted Treeline games played against AI. */
+    BOT_3x3,
+    /** Summoner's Rift ranked solo queue games. */
+    RANKED_SOLO_5x5,
+    /** Twisted Treeline ranked team games. */
+    RANKED_TEAM_3x3,
+    /** Summoner's Rift ranked team games. */
+    RANKED_TEAM_5x5,
+    /** One for All games. */
     ONEFORALL_5x5,
+    /** Snowdown Showdown 1x1 games. */
     FIRSTBLOOD_1x1,
+    /** Snowdown Showdown 2x2 games. */
     FIRSTBLOOD_2x2,
+    /** Hexakill games. */
     SR_6x6,
-    CAP_5x5
+    /** Team Builder games. */
+    CAP_5x5,
+    /** Ultra Rapid Fire games. */
+    URF,
+    /** Ultra Rapid Fire games played against AI. */
+    URF_BOT,
+    /** Summoner's Rift games played against Nightmare AI. */
+    NIGHTMARE_BOT,
+    /** Ascension games. */
+    ASCENSION
 }
--- src/main/java/com/achimala/leaguelib/models/PlayerStatSummaryType.java
+++ src/main/java/com/achimala/leaguelib/models/PlayerStatSummaryType.java
@@ -16,23 +16,52 @@
 
 package com.achimala.leaguelib.models;
 
+/**
+ * @see <a href="https://developer.riotgames.com/docs/game-constants">Game Constants</a>
+ */
 public enum PlayerStatSummaryType {
-    CAP1x1,
-    CAP5x5,
-    CoopVsAI,
-    CoopVsAI3x3,
-    CoopVsAI5x5,
-    OdinUnranked,
+    /** Summoner's Rift unranked games. */
     Unranked,
+    /** Twisted Treeline unranked games. */
     Unranked3x3,
+    /** Dominion/Crystal Scar games. */
+    OdinUnranked,
+    /** ARAM / Howling Abyss games. */
+    AramUnranked5x5,
+    /** Summoner's Rift and Crystal Scar games played against AI. */
+    CoopVsAI,
+    /** Twisted Treeline games played against AI. */
+    CoopVsAI3x3,
+    /** Summoner's Rift ranked solo queue games. */
     RankedSolo5x5,
+    /** Twisted Treeline ranked team games. */
     RankedTeam3x3,
+    /** Summoner's Rift ranked team games. */
     RankedTeam5x5,
+    /** One for All games. */
+    OneForAll5x5,
+    /** Snowdown Showdown 1x1 games. */
+    FirstBlood1x1,
+    /** Snowdown Showdown 2x2 games. */
+    FirstBlood2x2,
+    /** Hexakill games. */
+    SummonersRift6x6,
+    /** Team Builder games. */
+    CAP5x5,
+    /** Ultra Rapid Fire games. */
+    URF,
+    /** Ultra Rapid Fire games played against AI. */
+    URFBots,
+    /** Summoner's Rift games played against Nightmare AI. */
+    NightmareBot,
+    /** Ascension games. */
+    Ascension,
+    CAP1x1,
+    CoopVsAI5x5,
     RankedPremade3x3,
     RankedPremade5x5,
     AramUnranked1x1,
     AramUnranked2x2,
     AramUnranked3x3,
-    AramUnranked5x5,
     AramUnranked6x6
 }
