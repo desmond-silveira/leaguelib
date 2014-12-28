dfb6c825b203e840a11bf4f89e71ac93c6a6a1aa
Desmond Silveira <dsilveira@local.com>
2014-12-28 08:53:27 -0800

    Added LeagueMatchmakingQueue display name.

--- src/main/java/com/achimala/leaguelib/models/LeagueMatchmakingQueue.java
+++ src/main/java/com/achimala/leaguelib/models/LeagueMatchmakingQueue.java
@@ -21,46 +21,56 @@ package com.achimala.leaguelib.models;
  */
 public enum LeagueMatchmakingQueue {
     /** Custom games. */
-    NONE,
+    NONE ("Custom Game"),
     /** Summoner's Rift unranked games. */
-    NORMAL,
+    NORMAL ("Normal 5v5"),
     /** Twisted Treeline unranked games. */
-    NORMAL_3x3,
+    NORMAL_3x3 ("Normal 3v3"),
     /** Dominion/Crystal Scar games. */
-    ODIN_UNRANKED,
+    ODIN_UNRANKED ("The Crystal Scar"),
     /** ARAM / Howling Abyss games. */
-    ARAM_UNRANKED_5x5,
+    ARAM_UNRANKED_5x5 ("Howling Abyss"),
     /**
      * Summoner's Rift and Crystal Scar games played against Intro, Beginner, or
      * Intermediate AI.
      */
-    BOT,
+    BOT ("Co-op vs. AI 5v5"),
     /** Twisted Treeline games played against AI. */
-    BOT_3x3,
+    BOT_3x3 ("Co-op vs. AI 3v3"),
     /** Summoner's Rift ranked solo queue games. */
-    RANKED_SOLO_5x5,
+    RANKED_SOLO_5x5 ("Ranked Solo 5v5"),
     /** Twisted Treeline ranked team games. */
-    RANKED_TEAM_3x3,
+    RANKED_TEAM_3x3 ("Ranked Team 3v3"),
     /** Summoner's Rift ranked team games. */
-    RANKED_TEAM_5x5,
+    RANKED_TEAM_5x5 ("Ranked Team 5v5"),
     /** One for All games. */
-    ONEFORALL_5x5,
+    ONEFORALL_5x5("ONEFORALL_5x5"),
     /** Snowdown Showdown 1x1 games. */
-    FIRSTBLOOD_1x1,
+    FIRSTBLOOD_1x1("FIRSTBLOOD_1x1"),
     /** Snowdown Showdown 2x2 games. */
-    FIRSTBLOOD_2x2,
+    FIRSTBLOOD_2x2("FIRSTBLOOD_2x2"),
     /** Hexakill games. */
-    SR_6x6,
+    SR_6x6("SR_6x6"),
     /** Team Builder games. */
-    CAP_5x5,
+    CAP_5x5("CAP_5x5"),
     /** Ultra Rapid Fire games. */
-    URF,
+    URF("URF"),
     /** Ultra Rapid Fire games played against AI. */
-    URF_BOT,
+    URF_BOT("URF_BOT"),
     /** Summoner's Rift games played against Nightmare AI. */
-    NIGHTMARE_BOT,
+    NIGHTMARE_BOT("NIGHTMARE_BOT"),
     /** Ascension games. */
-    ASCENSION,
+    ASCENSION("ASCENSION"),
     /** Twisted Treeline 6x6 Hexakill games. */
-    HEXAKILL
+    HEXAKILL("HEXAKILL");
+    
+    private String _displayName;
+
+    private LeagueMatchmakingQueue(String displayName) {
+        _displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return _displayName;
+    }
 }
